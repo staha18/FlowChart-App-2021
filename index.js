7f5817940593f948e71184b4efc525693c104327
Andrey13-cyber <pescherov13@gmail.com>
2021-07-20 10:11:22 +0400

    file

---
+++ index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const url = require('url');
+const { app, BrowserWindow } = require('electron');
+const { ppid } = require('process');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 600,
+        height: 800,
+        fullscreen: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('generation.html');
+};
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
